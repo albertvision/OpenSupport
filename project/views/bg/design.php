04da53e5b612826f6586e79210009b043d23befd
Hristo Dimitrov <me@buhala.uchenici.bg>
2013-07-26 09:29:55 +0300

    Footer

--- project/views/bg/design.php
+++ project/views/bg/design.php
@@ -42,8 +42,10 @@
                 </div>
 
                 <div class="clear"></div>
-            </div>
 
+            </div>
+            <div id="footer">&copy; <?= date('Y') ?> All rights reserved<br>
+                <u><a href="http://opensupport.co">OpenSupport</a></u> created by AVB inc.</div>
         </div>
     </body>
 </html>
